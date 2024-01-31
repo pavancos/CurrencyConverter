80a962d1b0806046de0fce2566ae27a7528d48c1
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-01-31 22:44:59 +0530

    add:analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import CurrencyConverter from "./components/CurrencyConverter";
-
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <div className="App">
       <CurrencyConverter />
+      <Analytics/>
     </div>
   );
 }
